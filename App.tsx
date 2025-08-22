2841a90ae9460b049662c3da90bb2a151cce9cdd
Daniel Jacobson <danielyjacobson@gmail.com>
2025-08-21 19:10:26 -0500

    Fixed issue with background color

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-import OTPInput from './src/components/OTPInput/OTPInput';
+import OTPInput from "./src/components/OTPInput/OTPInput";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={["#ffffffff", "#ff9865ff"]}
+      start={[0, 0]}
+      end={[1, 1]}
+      style={styles.container}
+    >
       <OTPInput />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
